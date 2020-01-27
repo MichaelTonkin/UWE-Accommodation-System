0004860b5595a01bd4629567ed03e4996bc0d889
c7-francis <57913936+c7-francis@users.noreply.github.com>
2020-01-27 20:28:07 +0000

    Add files via upload
    Hall Manager Class

---
+++ src/uweaccommodationsystem/HallManager.java
@@ -0,0 +1,16 @@
+
+public class HallManager {
+    
+    String uerName;
+    String password;
+    
+    public String getPassword(){
+        
+        return this.password;
+    }
+    
+    public String getUserName(){
+        return this.uerName;
+    }
+    
+}
