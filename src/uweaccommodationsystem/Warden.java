3d91f084eda0c699247aa3eeaf8114430076931a
c7-francis <57913936+c7-francis@users.noreply.github.com>
2020-01-27 20:30:18 +0000

    Warden Class

---
+++ src/uweaccommodationsystem/Warden.java
@@ -0,0 +1,16 @@
+
+public class Warden {
+    String password;
+    String userName;
+    
+    public String getUsrename(){
+        
+        return this.userName;
+        
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+}
