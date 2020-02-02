29d723b99d6ca906aa5cf17a4b8727a78a108fc1
basasia <60352487+basasia@users.noreply.github.com>
2020-02-02 00:43:16 +0000

    Add files via upload

---
+++ gui login/FXMLTableViewController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiforuweaccommodationsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author asia
+ */
+public class FXMLTableViewController implements Initializable {
+ 
+    public void ChangeScreenButtonClicked(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLLoginView.fxml"));
+        
+        Scene scene = new Scene(root);
+        //set stage to window and get stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setTitle("UWEAccommodationSystem");
+        window.setScene(scene);
+        window.show();
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
