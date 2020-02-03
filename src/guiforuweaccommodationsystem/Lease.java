97508326571890654accc5868d66070173aa4eb3
Michael Tonkin <michaeltonkin99@gmail.com>
2020-02-03 14:54:35 +0000

    Add files via upload

---
+++ src/guiforuweaccommodationsystem/Lease.java
@@ -0,0 +1,69 @@
+/*
+Class: UWEAccommodationSystem
+Description: A lease is an object which can be identified with a room.
+    This class contains all pertenent information for the lease, such as 
+    the rent, address and student.
+Created: 28/01/2020
+Updated: 28/01/2020
+Authors: Michael Tonkin (Michael2.Tonkin@live.uwe.ac.uk)
+*/
+
+
+package guiforuweaccommodationsystem;
+
+
+public class Lease {
+    
+    private int duration;
+    private Student student;
+    private String leaseID;
+    private String address;
+    private double rentRateMonthly;
+    
+    /*
+    Function: Lease (constructor)
+    Description: creates and initialises the lease object when called
+    Parameters: duration - how long the lease is valid for.
+                leaseID - a unique id for the lease object.
+                addresss - the address associated with the lease.
+    */
+    public Lease(int duration, String leaseID, String address)
+    {
+        this.duration = duration;
+        this.leaseID = leaseID;
+        this.address = address;
+    }
+    
+    //We set the student associated with the lease here.
+    //The student class can be found in the same package,
+    //uweaccommodationsystem.
+    public void setStudent(Student student)
+    {
+        this.student = student;
+    }
+    
+    public void setRentRate(double rentRate)
+    {
+        this.rentRateMonthly = rentRate;
+    }
+    
+    public double getRentRate()
+    {
+        return rentRateMonthly;
+    }
+    
+    public String getLeaseID()
+    {
+        return leaseID;
+    }
+    
+    public int getDuration()
+    {
+        return duration;
+    }
+            
+    public String getAddress()
+    {
+        return address;
+    }
+}
---
+++ src/guiforuweaccommodationsystem/Warden.java
@@ -0,0 +1,18 @@
+package guiforuweaccommodationsystem;
+
+
+public class Warden {
+    private String password;
+    private String userName;
+    
+    public String getUsrename(){
+        
+        return this.userName;
+        
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+}
