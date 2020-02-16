b155c58963055352854bdb81e988245c25be485a
Michael Tonkin <michaeltonkin99@gmail.com>
2020-02-16 08:48:31 +0000

    Student names finished.

--- src/uweaccommodationsystem/UWEAccommodationSystem.java
+++ src/uweaccommodationsystem/UWEAccommodationSystem.java
@@ -27,68 +27,63 @@ public class UWEAccommodationSystem {
     private Room[] room = new Room[12];
     private Random rand = new Random();
     private static char accountType;
-    private static ArrayList<Property> property = new ArrayList<Property>();    
+    private static ArrayList<Property> property = new ArrayList<Property>();
     private int leaseNum = 1;
+    private int studentNameInc = 0;
     private int studentID = 18034567;
-    private String[] firstNames = {"James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib"
-, "James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib"
-, "James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib"};
-    
+    private String[] names = {"James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib",
+         "James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib",
+         "James", "Wei", "Bas", "Raj", "Chris", "Mr Sparkle", "Cat", "Bird", "Dog", "Ryan the Tiger", "Jon Snow", "Rakib"};
+
     //TODO UPDATE COMMENTS!!!
-    private void addRoomsAndLeases(Hall hall)
-    {
-        
+    private void addRoomsAndLeases(Hall hall) {
+
         int random = rand.nextInt(2);
         // Add rooms
         for (int x = 0; x < room.length; x++) {
             room[x] = new Room(x);
-                    
+
             //Create lease if random number is 1, else do nothing
-            
+            random = rand.nextInt(2);
+            if (random == 1) {
+                //leaseNum = leaseNum + x;
+                lease.add(new Lease(12, leaseNum, Integer.toString(x) + " Frenchay Campus, Bristol, BS16 1ZG")); //TODO add hall name
+                //if room has a lease set cleaningstatus and occupancy to Clean and Occupied
                 random = rand.nextInt(2);
-                if (random == 1)
-                {
-                    //leaseNum = leaseNum + x;
-                    lease.add(new Lease(12, leaseNum, Integer.toString(x) + " Frenchay Campus, Bristol, BS16 1ZG")); //TODO add hall name
-                    //if room has a lease set cleaningstatus and occupancy to Clean and Occupied
-                    random = rand.nextInt(2);
-                    //set the cleaning status randomly
-                    switch(random)
-                            {
-                            case 1: room[x].setCleaningStatus("Clean");
-                                    break;
-                            default: room[x].setCleaningStatus("dirty");
-                            }
-                                room[x].setRoomStatus("Occupied");
-                    
+                //set the cleaning status randomly
+                switch (random) {
+                    case 1:
+                        room[x].setCleaningStatus("Clean");
+                        break;
+                    default:
+                        room[x].setCleaningStatus("Dirty");
                 }
-                else
-                {
-                    lease.add(new Lease(0, 0, "")); //TODO 0 = empty
-                    //if room has a lease set cleaningstatus and occupancy to Offline and Unccupied
-                    room[x].setCleaningStatus("Offline");
-                    room[x].setRoomStatus("Unoccupied");
-                }
-                random = rand.nextInt(1000);
-                
-                System.out.println(leaseNum - 1);
-                System.out.println(firstNames.length);
-                student.add(new Student(studentID + random, firstNames[leaseNum - 1])); //TODO random student generator
+                student.add(new Student(studentID + random, names[leaseNum - 1]));
                 lease.get(leaseNum - 1).setStudent(student.get(leaseNum - 1));
 
+                room[x].setRoomStatus("Occupied");
                 
-                room[x].setLease(lease.get(leaseNum - 1));
-  
+
+            } else {
+                lease.add(new Lease(0, 0, "")); //TODO 0 = empty
                 
-                leaseNum++;
-                
+                student.add(new Student(0 + random, " "));
+                lease.get(leaseNum - 1).setStudent(student.get(leaseNum - 1));
+                //if room has a lease set cleaningstatus and occupancy to Offline and Unccupied
+                room[x].setCleaningStatus("Offline");
+                room[x].setRoomStatus("Unoccupied");
+            }
+            random = rand.nextInt(1000);
+
+            room[x].setLease(lease.get(leaseNum - 1));
+
+            leaseNum++;
+
             //}
-            
-          
             hall.addRoom(room[x]);
         }
     }
-    
+
     /*
     Constructor: UWEAccommodationSystem
     Description: Intialises halls, adds rooms to halls and has a chance to create a new lease for a room based on a random number.
@@ -100,34 +95,32 @@ public class UWEAccommodationSystem {
         Hall hall = new Hall("H1", 1, "James Town, Frenchay Campus, Bristol, BS16 1ZG", "0123 177777");
         Hall hall2 = new Hall("H2", 2, "Kun Town, Frenchay Campus, Bristol, BS16 1ZG", "0123 188888");
         Hall hall3 = new Hall("H3", 3, "Jim Town, Frenchay Campus, Bristol, BS16 1ZG", "0123 199999");
-               
+
         addRoomsAndLeases(hall);
-        addRoomsAndLeases(hall2);        
+        addRoomsAndLeases(hall2);
         addRoomsAndLeases(hall3);
-        
+
         this.addHall(hall);
         this.addHall(hall2);
         this.addHall(hall3);
-        
-        for(int i = 0; i < halls.size(); i++)
-        {
+
+        for (int i = 0; i < halls.size(); i++) {
             //loop through each hall to get the rooms
-            for(int j = 0; j < halls.get(i).getRooms().size(); j++)
-            {
-                property.add( new Property(
-                        halls.get(i).getHallName(), 
-                        halls.get(i).getHallNum(), 
-                        halls.get(i).getHallAddress(), 
+            for (int j = 0; j < halls.get(i).getRooms().size(); j++) {
+                property.add(new Property(
+                        halls.get(i).getHallName(),
+                        halls.get(i).getHallNum(),
+                        halls.get(i).getHallAddress(),
                         halls.get(i).getHallTel(),
                         halls.get(i).getRooms().get(j).getRoomNumber(),
-                        halls.get(i).getRooms().get(j).getLease().getLeaseID(), 
+                        halls.get(i).getRooms().get(j).getLease().getLeaseID(),
                         halls.get(i).getRooms().get(j).getLease().getDuration(),
                         halls.get(i).getRooms().get(j).getLease().getRentRate(),
                         halls.get(i).getRooms().get(j).getLease().getStudent().getStudentName(),
-                        halls.get(i).getRooms().get(j).getLease().getStudent().getStudentID(),       
+                        halls.get(i).getRooms().get(j).getLease().getStudent().getStudentID(),
                         halls.get(i).getRooms().get(j).getRoomStatus(),
                         halls.get(i).getRooms().get(j).getCleaningStatus()
-                        ));
+                ));
             }
         }
     }
@@ -172,9 +165,8 @@ public class UWEAccommodationSystem {
     public static void addHall(Hall hall) {
         halls.add(hall);
     }
-    
-    public ArrayList getProperty()
-    {
+
+    public ArrayList getProperty() {
         return this.property;
     }
 }
